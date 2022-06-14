0d628272169da69c9c27ae53dc185814367e6fce
Romain Birling <rombirli@gmail.com>
2022-06-14 19:05:37 +0200

    Added chunk system

---
+++ Assets/Scripts/ChunkLoader.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChunkLoader : MonoBehaviour
+{
+    public List<GameObject> chunks;
+
+    public float chunkWidth = 6.5f, chunkHeight = 4;
+    private readonly Dictionary<Tuple<int, int>, GameObject> _cache = new();
+
+    // Update is called once per frame
+    private int lastIntersectionX = 0, lastIntersectionY = 0;
+
+    private void Start()
+    {
+        _cache.Add(new Tuple<int, int>(0, 0), new GameObject("Empty chunk - spawn"));
+        UpdateChunks(0, 0);
+    }
+
+    void Update()
+    {
+        // 0 - 14,14 
+        var playerPos = GameObject.FindWithTag("Player").transform.position;
+        var x = (int)(playerPos.x / chunkWidth - (playerPos.x < 0 ? 1 : 0));
+        var y = (int)Math.Round(playerPos.y / chunkHeight - (playerPos.y < 0 ? 1 : 0));
+        if (x == lastIntersectionX && y == lastIntersectionY) return;
+        lastIntersectionX = x;
+        lastIntersectionY = y;
+        UpdateChunks(x, y);
+    }
+
+    private void UpdateChunks(int intersectionX, int intersectionY)
+    {
+        foreach (var chunk in _cache.Values) chunk.SetActive(false);
+        for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            LoadChunk(intersectionX + i, intersectionY + j);
+    }
+
+    private void LoadChunk(int x, int y)
+    {
+        GameObject chunkToLoad;
+        var coord = new Tuple<int, int>(x, y);
+        if (!_cache.TryGetValue(coord, out chunkToLoad))
+        {
+            var position = new Vector3(chunkWidth * x, chunkHeight * y, 0);
+            var index = Random.Range(0, chunks.Count);
+            chunkToLoad = Instantiate(chunks[index], position, Quaternion.identity);
+            _cache.Add(coord, chunkToLoad);
+        }
+
+        chunkToLoad.SetActive(true);
+    }
+}
