424219b44f85875a0f9a00911dd7fa50f0ebf761
Romain Birling <rombirli@gmail.com>
2022-06-18 00:40:51 +0200

    Progress on inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Inventory
+{
+    public enum Item
+    {
+        Coin,
+        Star,
+        Key,
+        Health
+    }
+
+    private static readonly Dictionary<Item, int> ItemToCount =
+        LoadItemToCount();
+
+
+    private static readonly Dictionary<Item, int> ItemToCapacity =
+        LoadItemToCapacity();
+
+    private static Dictionary<Item, int> LoadItemToCount() =>
+        ((int[])Enum.GetValues(typeof(Item))).ToDictionary(item => (Item)item, item => 0);
+
+    private static Dictionary<Item, int> LoadItemToCapacity() =>
+        ((int[])Enum.GetValues(typeof(Item))).ToDictionary(item => (Item)item, item => 0);
+
+
+    public static int GetCount(Item item) => ItemToCount[item];
+    private static int GetCapacity(Item item) => ItemToCapacity[item];
+
+    public static bool Put(Item item)
+    {
+        if (GetCount(item) >= GetCapacity(item))
+        {
+            ItemToCount[item]++;
+            return true;
+        }
+        else return false;
+    }
+
+
+    public static bool Pop(Item item)
+    {
+        if (ItemToCount[item] > 0)
+        {
+            ItemToCount[item]--;
+            return true;
+        }
+        else return false;
+    }
+}
