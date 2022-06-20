aaddee4951eb5bc76e338c417f3a897965ea7706
Romain Birling <rombirli@gmail.com>
2022-06-20 13:52:16 +0200

    Added unversioned assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager
+{
+    public static void Pause() => Time.timeScale = 0;
+
+    public static void Resume() => Time.timeScale = 1;
+}
